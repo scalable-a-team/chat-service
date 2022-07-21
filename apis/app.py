7b80a2db3217300f3ff63009bc2058af994096cb
Boss Udomsuktham <bossqi@Bosss-MacBook-Pro.local>
2022-07-22 01:02:22 +0700

    fixing docker image

--- apis/app.py
+++ apis/app.py
@@ -15,7 +15,7 @@ CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 MONGO_HOST = os.environ['DB_HOST']
 MONGO_PORT = os.environ['DB_PORT']
-MONGO_DRIVE = f'mongodb://{MONGO_HOST:MONGO_PORT}'
+MONGO_DRIVE = f'mongodb://{MONGO_HOST}:{MONGO_PORT}'
 print('-----------')
 print(MONGO_DRIVE)
 print('-----------')
